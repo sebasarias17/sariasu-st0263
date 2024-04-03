29a173369c14de9893cec8ffdcc6742288a8d5d3
seabsarias17 <sebasrias33314@gmail.com>
2024-04-02 23:27:50 -0500

    particionamiento y decode arreglado

--- Proyecto 1/NameNode.py
+++ Proyecto 1/NameNode.py
@@ -1,6 +1,6 @@
 import os
 
-def dividir_archivo(ruta_archivo, tamaño_bloque, ruta_particiones):
+def dividir_archivo(ruta_archivo, tamaño_bloque):
     nombre_archivo = os.path.basename(ruta_archivo)
     archivos =[]
     
@@ -14,10 +14,8 @@ def dividir_archivo(ruta_archivo, tamaño_bloque, ruta_particiones):
                 numero_parte += 1
                 nombre, extension = os.path.splitext(nombre_archivo)
                 nombre_nuevo_archivo = f'{nombre}_{numero_parte}{extension}'
-                ruta_nuevo_archivo = os.path.join(ruta_particiones, nombre_nuevo_archivo)
-                with open(ruta_nuevo_archivo, 'wb') as archivo_parte:
+                with open(nombre_nuevo_archivo, 'wb') as archivo_parte:
                     archivo_parte.write(fragmento)
-                    archivos.append(ruta_nuevo_archivo)
                     archivos.append(fragmento)
                 print(f'Creado {nombre_nuevo_archivo}')
             return archivos
@@ -26,28 +24,18 @@ def dividir_archivo(ruta_archivo, tamaño_bloque, ruta_particiones):
     else:
         print('Particionamiento de archivo completado.')
 
-#def juntar_archivo(archivos):
-#    archivos_content= b""
-#    for _ in archivos:
-#        archivos_content += _
-#    with open('archivo_nuevo.docx', 'wb') as f:
-#        f.write(archivos_content)
-
-def juntar_archivo(archivos, ruta_unificado):
-    archivos_content = b""  
-    for fragmento in archivos:
-        if isinstance(fragmento, bytes): 
-            archivos_content += fragmento
-        else:
-            archivos_content += fragmento.encode('utf-8')
-    with open(os.path.join(ruta_unificado,'archivo_nuevo.docx'), 'wb') as f:
-        f.write(archivos_content)
+def juntar_archivo(archivos):
+   archivos_content= b""
+   for _ in archivos:
+       archivos_content += _
+   with open('archivo_nuevo.docx', 'wb') as f:
+       f.write(archivos_content)
 
 
 # Uso de la función
 RUTA_ARCHIVO = './Archivo1.docx'
 TAMAÑO_BLOQUE = 2048 
-RUTA_PARTICIONES = './Particiones'
-RUTA_UNIFICADO = './Archivos Unificados'
-array = dividir_archivo(RUTA_ARCHIVO, TAMAÑO_BLOQUE, RUTA_PARTICIONES)
-juntar_archivo(array, RUTA_UNIFICADO)
+# RUTA_PARTICIONES = './Particiones'
+# RUTA_UNIFICADO = './Archivos Unificados'
+array = dividir_archivo(RUTA_ARCHIVO, TAMAÑO_BLOQUE)
+juntar_archivo(array)
