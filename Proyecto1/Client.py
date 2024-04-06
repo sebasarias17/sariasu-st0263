0be489f3f5ab7b3d96055b06f57f9033dc6b6008
Jeronimo Gonzalez <jerog2015@gmail.com>
2024-04-05 20:55:45 -0500

    Biblioteca y Listar

--- Proyecto1/Client.py
+++ Proyecto1/Client.py
@@ -1,39 +1,46 @@
 import grpc
 import Service_pb2
 import Service_pb2_grpc
-from concurrent import futures
 import os
 
 def dividir_archivo(ruta_archivo):
     nombre_archivo = os.path.basename(ruta_archivo)
     archivos = []
     peso_archivo = os.path.getsize(ruta_archivo)
-    print(peso_archivo)
     chunks_bytes = 3 * 1024 * 1024
-    espacio_chunk = ((peso_archivo // chunks_bytes) +1 )* 1024 * 1024
-    print(espacio_chunk)
+    espacio_chunk = ((peso_archivo // chunks_bytes) + 1) * 1024 * 1024
+
     try:
         with open(ruta_archivo, 'rb') as archivo:
             numero_parte = 0
             while True:
                 fragmento = archivo.read(espacio_chunk)
-                archivos.append(fragmento)
                 if not fragmento:
                     break
+                archivos.append((fragmento, numero_parte))
                 numero_parte += 1
-        return archivos
+        return archivos, nombre_archivo
     except IOError as error:
         print(f'Error: {error}')
+        return [], nombre_archivo
 
 with grpc.insecure_channel('localhost:50051', options=[
     ('grpc.max_send_message_length', 100 * 1024 * 1024),
-    ('grcp.max_receive_message_length', 100 * 1024 * 1024)
+    ('grpc.max_receive_message_length', 100 * 1024 * 1024)
     ]) as channel:
 
     stub = Service_pb2_grpc.ClientCallServiceStub(channel)
-    arreglo = dividir_archivo('./image.jpg')
-    response = stub.sendPartitionFile(Service_pb2.File(content = arreglo))
-    print(response.requestFile)
+    chunks, nombre_archivo = dividir_archivo('./image.jpg')
+    for chunk, numero_parte in chunks:
+        response = stub.sendPartitionFile(Service_pb2.File(
+            content=chunk,
+            fileName=nombre_archivo,
+            partNumber=numero_parte
+        ))
+        print(response.requestFile)
+
+    stored_chunks = stub.ListStoredChunks(Service_pb2.Empty())
+    print("Chunks almacenados:", stored_chunks.chunks)
 
 # def juntar_archivo(archivos, carpeta_uni = 'Archivos Unificados'):
 
